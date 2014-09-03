abcb311fe2aeeb0457c202ca704a140b884977ea
dye E. jarhoo <gene0mega@zoho.com>
2014-09-03 09:30:11 +0800

    fix stuffs, provide cssSwitch @nested quotes -dye
    fix scrolling speed, remove refView, add betterquote style switch, add hit quoteSize limit title attribution

--- hacer.meta.js
+++ hacer.meta.js
@@ -2,7 +2,7 @@
 // @name hacer
 // @namespace https://github.com/doomred
 // @description Provide a better HTML architecture for add functions and CSS design. This script is by hacers for hacers.
-// @version 0.0.5
+// @version 0.0.6
 // @license ISC
 // @copyleft dye E. jarhoo
 // @icon https://raw.github.com/doomred/hacer/master/hacer_icon.png
